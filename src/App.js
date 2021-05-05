6c9e99742c5bb01799fb489e83a005bc0c65a4a7
DawidWebb <tomasz.matras@developerweb.pl>
2021-05-05 13:23:04 +0200

    feat: add Header, Button and Spinner components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { HashRouter as Router } from "react-router-dom";
+import { Fragment } from "react";
+import Spinner from "./components/Spinner/Spinner";
+import Header from "./vievs/Header/Header";
+import MainSwitch from "./components/MainSwitch/MainSwitch";
+import React from "react";
+
 function App() {
   return (
-    <div>
-      <p>test</p>
-    </div>
+    <Router>
+      <Header />
+      <Fragment>
+        <MainSwitch />
+      </Fragment>
+    </Router>
   );
 }
 
-export default App;
+function RootApp() {
+  return (
+    <React.Suspense fallback={<Spinner />}>
+      <App />
+    </React.Suspense>
+  );
+}
+
+export default RootApp;
---
+++ src/vievs/Header/Header.js
@@ -0,0 +1,20 @@
+import Button from "../../components/Buttons/Button";
+
+import styles from "./header.module.scss";
+
+const Header = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.title}>
+        <h1>Faktury onLine</h1>
+      </div>
+
+      <div className={styles.options}>
+        <Button name="zaloguj" />
+        <Button name="testuj" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
