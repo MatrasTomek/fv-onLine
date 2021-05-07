efe5e440365dd00c3ea3ed054ec742bec85036b1
DawidWebb <tomasz.matras@developerweb.pl>
2021-05-07 14:00:38 +0200

    feat: set op index for all components folders

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Button } from "./Buttons";
 export { default as Spinner } from "./Spinner";
 export { default as MainSwitch } from "./MainSwitch";
+export { default as Modal } from "./Modal";
