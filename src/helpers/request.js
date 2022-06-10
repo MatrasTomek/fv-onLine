1c0c6672e8e08091cced6d50d8f9329dd057eb7a
DawidWebb <tomasz.matras@developerweb.pl>
2022-06-10 10:40:26 +0200

    change baseUrl

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const request = axios.create({
-	baseURL: "https://invoice-nest.herokuapp.com/",
-	// baseURL: "http://localhost:8080",
+	// baseURL: "https://invoice-nest.herokuapp.com/",
+	baseURL: "http://localhost:8080",
 
 	validateStatus: false,
 });
