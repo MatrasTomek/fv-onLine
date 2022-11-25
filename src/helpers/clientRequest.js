847428298492fc01a211b8783150723bd347f389
DawidWebb <tomasz.matras@developerweb.pl>
2022-11-25 13:08:29 +0100

    update client request for B4A

--- src/helpers/clientRequest.js
+++ src/helpers/clientRequest.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const clientRequest = axios.create({
-  baseURL: "https://order-on-line.herokuapp.com/",
-
-  validateStatus: false,
+	// baseURL: "https://order-on-line.herokuapp.com/",
+	baseURL: "https://ordersback.b4a.app",
+	validateStatus: false,
 });
 
 export default clientRequest;
